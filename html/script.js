f05270bd25cee889800014c2830ce93bc8419c63
PetitsPieds <76625834+Petits-Pieds@users.noreply.github.com>
2021-10-15 18:15:36 -0400

    Update

--- html/script.js
+++ html/script.js
@@ -2,78 +2,63 @@ let ButtonsData = [];
 let Buttons = [];
 let Button = [];
 
-const OpenMenu = (data) => {
-    DrawButtons(data);
-}
+window.addEventListener("message", (event) => {
+    if (event.data.type === "open_context") {
+        CreateContext(event.data.data)
+    };
+});
 
-const CloseMenu = () => {
+window.addEventListener("click", (event) => {
+    let $target = $(event.target);
+    if ($target.closest(".btn").length && $(".btn").is(":visible")) {
+        let id = event.target.id;
+        if (!Button[id]) return 
+        OnClick(id);
+    }
+});
+
+window.addEventListener("keyup", (event) => {
+    switch(event.which) {
+        case 8:
+            CancelContext();
+            break;
+        case 27:
+            CancelContext();
+        return;
+    }
+});
+
+function CreateContext(data) {
+    ButtonsData = data;
+    for (let i = 0; i < ButtonsData.length; i++) {
+        let id = ButtonsData[i].id;
+        let title = ButtonsData[i].title;
+        let description = ButtonsData[i].description;
+        let element = $(`<div class="btn" id=` + id +`><div class="title" id=` + id +`>` + title + `</div><div class="description" id=` + id + `>` + description + `</div></div>`);
+        $("#buttons").append(element);
+        Buttons[id] = element;
+        if (ButtonsData[i].settings) {
+            Button[id] = ButtonsData[i].settings
+        };
+    };
+};
+
+function CloseContext() {
     for (let i = 0; i < ButtonsData.length; i++) {
         let id = ButtonsData[i].id
-        $(".button").remove();
-    };
+        $(".btn").remove();
+    }
     ButtonsData = [];
     Buttons = [];
     Button = [];
 };
 
-const DrawButtons = (data) => {
-    ButtonsData = data;
-    for (let i = 0; i < ButtonsData.length; i++) {
-        let header = ButtonsData[i].header;
-        let message =  ButtonsData[i].txt || ButtonsData[i].text;
-        let id = ButtonsData[i].id;
-        let element;
-
-        element = $(`
-            <div class="button" id=` + id + `>
-                <div class="header" id=` + id + `>` + header + `</div>
-                <div class="text" id=` + id + `>` + message + `</div>
-            </div>`);
-        $('#buttons').append(element);
-        Buttons[id] = element;
-        if (ButtonsData[i].params) {
-            Button[id] = ButtonsData[i].params
-        };
-    };
+function CancelContext() {
+    $.post(`https://dl-context/CloseContext`)
+    CloseContext();
 };
 
-$(document).click(function (event) {
-    let $target = $(event.target);
-    if ($target.closest('.button').length && $('.button').is(":visible")) {
-        let id = event.target.id;
-        if (!Button[id]) return
-        PostData(id);
-    }
-});
-
-const PostData = (id) => {
-    $.post(`https://zf_context/clickedButton`, JSON.stringify(Button[id]));
-    return CloseMenu();
-};
-
-const CancelMenu = () => {
-    $.post(`https://zf_context/closeMenu`);
-    return CloseMenu();
-};
-
-window.addEventListener("message", (event) => {
-    const data = event.data;
-    const infos = data.data;
-    const action = data.action;
-    switch (action) {
-        case "OPEN_MENU":
-            return OpenMenu(infos);
-        case "CLOSE_MENU":
-            return CloseMenu();
-        default:
-            return;
-    };
-});
-
-document.onkeyup = function (event) {
-    event = event || window.event;
-    var charCode = event.keyCode || event.which;
-    if (charCode == 27) {
-        CancelMenu();
-    };
+function OnClick(id) {
+    $.post(`https://dl-context/OnClick`, JSON.stringify(Button[id]))
+    CloseContext();
 };
